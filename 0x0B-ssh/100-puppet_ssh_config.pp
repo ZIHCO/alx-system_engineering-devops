dba9e387ae2e0c8628283881fd5d020682c5bb69
ZIHCO <chizobajames21@gmail.com>
2023-01-13 01:19:41 +0000

    done

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,17 +1,16 @@
 # using puppet to set up config file for ssh
-include <stdlib>
-
+#include <stdlib>
 
 file_line { 'Turn off password':
   path    => '/etc/ssh/ssh_config',
-  ensure  => 'present',
+  ensure  => present,
   line    => 'PasswordAuthentication no',
   match   => '^PasswordAuthentication'
 }
 
 file_line { 'Identity file assignment':
   path    => '/etc/ssh/ssh_config',
-  ensure  => 'present',
+  ensure  => present,
   line    => 'IdentityFile ~/.ssh/school',
   match   => '^IdentityFile'
 }
