6d74e289dfc62fe9b9569fc0bbc5d6ef129dcfba
ZIHCO <chizobajames21@gmail.com>
2023-01-13 00:41:52 +0000

    edited

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -4,10 +4,5 @@ file { '/etc/ssh/config':
   ensure  => file,
   mode    => '600',
   owner   => 'ubuntu',
-  content => '# ssh_conf950 man page\n; Include /etc/ssh/ssh_config.d/*.conf\n; Host *\n;
-  PubKeyAuthentication yes\n;
-  IndentityFile ./.ssh/school\n;
-  GSSAPIAuthentication yes\n;
-  SendEnv LANG LC_*\n;
-  HashKnownHosts yes\n',
+  content => '# ssh_conf950 man page\n; Include /etc/ssh/ssh_config.d/*.conf\n; Host *\n; PubKeyAuthentication yes\n; IndentityFile ./.ssh/school\n; GSSAPIAuthentication yes\n; SendEnv LANG LC_*\n; HashKnownHosts yes\n',
 }
