a7196209a1eb422e30012507c5a2e04c800291e5
ZIHCO <chizobajames21@gmail.com>
2023-07-13 11:34:45 +0100

    done

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,39 @@
+# Puppet manifest containing commands to automatically configure an
+# Ubuntu machine to respect above requirements:
+# ngnix shouldbe installed and listen on port 80
+# When querying Nginx at its root / with a GET request (requesting a page)
+# using curl, it must return a page that contains the string Hello World!
+# The redirection must be a “301 Moved Permanently”.
+
+include stdlib
+
+exec { 'update apt':
+  command => '/usr/bin/apt-get update'
+}
+
+exec { 'nginx restart':
+  command => '/usr/sbin/service restart nginx',
+  require => Package['nginx']
+}
+
+package { 'nginx':
+  ensure  => installed,
+  require => Exec['update apt']
+}
+
+file { '/var/www/html/index.html':
+  ensure  => file,
+  mode    => '0744',
+  content => 'Hello World!',
+  owner   => 'root',
+  group   => 'root'
+}
+
+file_line { 'redirect_me':
+  ensure  => present,
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'server\ _;',
+  line    => 'location /redirect_me \{\n\t\treturn 301 https://twitter.com/jamesmatics;\n\t}',
+  notify  => Exec['restart nginx'],
+  require => File['/var/www/html/index.html']
+}
